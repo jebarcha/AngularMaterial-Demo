8b4f1fac06764d2389639593d82ee6a684cf51ee
jebarcha <jebarcha@hotmail.com>
2020-12-21 09:56:59 -0600

    angular flex-layout demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'demo', 
+    loadChildren: () => import('./demo/demo.module')
+      .then(m => m.DemoModule) 
+  },
+  { path: '**', redirectTo: 'demo'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
